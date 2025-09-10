7f3f077a2036ed1d96776223f2e69ec3f0719f6a
JosephJary <joseph.smith1@xtra.co.nz>
2025-09-10 18:09:34 +1200

    commiting first code have only done the html and a bit of css

---
+++ script.js
@@ -0,0 +1,8 @@
+//get the info from the textboxes
+function searchClicked(){
+    console.log("Search clicked");
+}
+//Resets whats in the textboxes.
+function resetClicked(){
+    console.log("reset clicked");
+}
