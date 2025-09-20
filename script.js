f271d737989fec9e457105a17ffc2111e095db61
JosephJary <joseph.smith1@xtra.co.nz>
2025-09-20 20:34:49 +1200

    fixed up the movie information making it look nice and made it so user can favourite movies

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 function displayMovieInfo(movieData){
     console.log(movieData);
 
+    //gets all the information from the api and put into variables
     const movieTitle = movieData.Title;
     const movieYear = movieData.Year;
     const movieRated = movieData.Rated;
@@ -16,7 +17,7 @@ function displayMovieInfo(movieData){
     const moviePlot = movieData.Plot;
     const movieAwards = movieData.Awards;
     const moviePoster = movieData.Poster;
-    const movieRatings = movieData.imdbRating; // or you can dig into Ratings array
+    const movieRatings = movieData.imdbRating; 
     const movieBoxOffice = movieData.BoxOffice;
 
      // Update HTML
@@ -38,7 +39,7 @@ function displayMovieInfo(movieData){
 
 
 function useAPI(title, year, plot){
-
+    //api searching with title year and plot
     const url = "http://www.omdbapi.com/?t=" + title + "&y=" + year + "&plot=" + plot + "&apikey=76eea785";
 
 
@@ -51,22 +52,32 @@ function useAPI(title, year, plot){
 
 //function to get the data that the user entered on the website
 function getUserInfo(){
+    //value from the search
     const title = document.getElementById("titleSearch").value;
     const year = document.getElementById("YearSearch").value;
     const plot = document.getElementById("plotSearch").value;
 
 
-    useAPI(title,year,plot);
-    console.log(title);
-    console.log(year);
-    console.log(plot);
+
+    //checks if title search box is empty
+    if (title === "") {
+        errorMessage.textContent = "⚠️ Please enter a movie title.";
+        errorMessage.style.display = "block"; // show the message
+    }else {//else display movie information and use the api
+        document.getElementById("movieInfo").style.display = "flex";
+        document.getElementById("movieH3").style.display = "block";
+        document.getElementById("movieDetails").style.display = "block";
+
+        errorMessage.style.display = "none";
+        useAPI(title, year, plot);
+    }
+
 }
 
 //get the info from the textboxes
 function searchClicked(){
     console.log("Search clicked");
-    document.getElementById("movieInfo").style.display = "flex";
-    document.getElementById("movieH3").style.display = "block";
+  
     getUserInfo();
 }
 //Resets whats in the textboxes.
@@ -95,3 +106,72 @@ function resetClicked(){
     document.getElementById("movieRatings").textContent = "";
     document.getElementById("movieBoxOffice").textContent = "";
 }
+//array used to store favourite movies
+let favourites = [];
+
+//adds movies to the favourite list
+function addToFavourites() {
+    const movie = {
+        title: document.getElementById("movieTitle").textContent,
+        year: document.getElementById("movieYear").textContent,
+        poster: document.getElementById("moviePoster").src,
+        plot: document.getElementById("moviePlot").textContent
+    };
+
+    // Avoid duplicates
+    if (!favourites.find(fav => fav.title === movie.title && fav.year === movie.year)) {
+        favourites.push(movie);
+        console.log(`${movie.title} has been added to favourites!`);
+        displayFavourites(); // update the list live
+    } else {
+        console.log(`${movie.title} is already in favourites!`);
+    }
+}
+
+
+//function to display all the favourite movies in the array
+function displayFavourites() {
+    const container = document.getElementById("favouriteMovies");
+    container.style.display = "flex"; //makes it visible
+    // Clear previous content except the heading
+    container.innerHTML = "<h3>Favourite Movies</h3>";
+    //if there are no movies favourited
+    if (favourites.length === 0) {
+        const msg = document.createElement("p");
+        msg.textContent = "No favourites yet!";
+        msg.style.color = "#555";
+        container.appendChild(msg);
+        return;
+    }
+
+
+    //displays all the movies in the array
+    favourites.forEach(movie => {
+        const movieDiv = document.createElement("div");
+        movieDiv.classList.add("favMovie");
+
+        // Poster
+        const poster = document.createElement("img");
+        poster.src = movie.poster || "";
+        poster.alt = movie.title;
+
+        // Details
+        const detailsDiv = document.createElement("div");
+        detailsDiv.classList.add("favMovieDetails");
+
+        const title = document.createElement("h4");
+        title.textContent = `${movie.title} (${movie.year})`;
+
+        const plot = document.createElement("p");
+        plot.textContent = movie.plot;
+
+        detailsDiv.appendChild(title);
+        detailsDiv.appendChild(plot);
+
+        movieDiv.appendChild(poster);
+        movieDiv.appendChild(detailsDiv);
+
+        container.appendChild(movieDiv);
+    });
+}
+
